2676d0d444a5b2d2df48774b1bb859c6d540a55a
Zaragoza2123 <zaragoza.coyote618@gmail.com>
2022-03-09 15:58:02 -0600

    lol

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import PersonCard from './people';
 function App() {
   return (
     <div className="App">
-      <PersonCard firstname = "Jane" lastname = "Doe" age = "45" hairColor = "Black"/>
-      <PersonCard firstname = "John" lastname = "Smith" age = "88" hairColor = "Brown"/>
-      <PersonCard firstname = "Millard" lastname = "Fillmore" age = "50" hairColor = "Brown"/>
-      <PersonCard firstname = "Maria" lastname = "Smith" age = "62" hairColor = "Brown"/>
+      <PersonCard firstname = "Jane" lastname = "Doe" age = {45} hairColor = "Black"/>
+      <PersonCard firstname = "John" lastname = "Smith" age = {88} hairColor = "Brown"/>
+      <PersonCard firstname = "Millard" lastname = "Fillmore" age = {50} hairColor = "Brown"/>
+      <PersonCard firstname = "Maria" lastname = "Smith" age = {62} hairColor = "Brown"/>
     </div>
   );
 }
--- src/people.js
+++ src/people.js
@@ -1,16 +1,34 @@
 import React, {Component} from 'react';
 
 class PersonCard extends Component {
+    //constructor allows to initalize other varibles to keep track of changing information 
+    constructor(props) {
+        super(props);
+        //create an object to 
+        this.state = {
+            //create a sate variable for age so it's not just in a props variable (which isnt allowed in react)
+            ages: this.props.age  //initialized the value of ages to be    what value we got from likes
+        };
+    this.addToBday = () =>  {
+        console.log("Its ya bday bruh")
+        this.setState({ages: this.state.ages + 1}) // in order to update state variables we must use this.setState()
+    }
+    }
+
+
     render() {
-        const {firstname, lastname, age, hairColor} = this.props;
+        //const {firstname, lastname, hairColor} = this.props;
         return (
         <div>
-            <h1>{lastname}, {firstname}</h1>
-            <p>Age: {age}</p>
-            <p>Hair Color: {hairColor}</p>
+            <h1>{this.props.lastname}, {this.props.firstname}</h1>
+            <p>Age: {this.state.ages}</p>
+            <p>Hair Color: {this.props.hairColor}</p>
+            <button onClick = {this.addToBday} >Birthday Button for {this.props.firstname} {this.props.lastname} </button>
+            <hr />
         </div>
         );
 
-    }
+
+}
 }
 export default PersonCard;
